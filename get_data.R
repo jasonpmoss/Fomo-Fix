d5566f1d3ad61ebd207e9ce01d36db283b15e71d
Jason <jason.moss@stern.nyu.edu>
2019-01-04 03:57:17 +0200

    Fixed get_data variable names

--- get_data.R
+++ get_data.R
@@ -5,10 +5,10 @@ get_dataframe <-function(sql_query){
   project <- "fomofix-217307" #project ID 
   output <- bq_project_query(project, sql, destination_table = NULL, quiet = NA)
   
-  reviews_big <- bq_table_download(output)
-  reviews_small<-reviews_big[1:10000,]
+  ratings_big <- bq_table_download(output)
+  ratings_small<-ratings_big[1:1000,]
   
-  return(list(full=reviews_big,small=reviews_small)) #two dataframe sizes while we work out our requirements/restrictions
+  return(list(full=ratings_big,small=ratings_small)) #two dataframe sizes while we work out our requirements/restrictions
 }
 
 #----------------------------------------testing the function-----------------------------------------------#
