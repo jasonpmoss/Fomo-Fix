7244ebf9036a39ee9a208532a2e8244e5530ea83
Jason <jason.moss@stern.nyu.edu>
2019-01-08 10:49:58 +0200

    Popularity based predictor - first version
    First version of the popularity based recommender complete. Outputs 3 recommendations. Need to figure out what the predict parameter ratings_matrix[250] means

---
+++ Popular_predict.R
@@ -0,0 +1,22 @@
+#Author: Jason
+
+library(dplyr)
+library(recommenderlab)
+
+Popular_train<-function(ratings_matrix){
+  # Start the clock!
+  ptm <- proc.time()
+  
+  rec=Recommender(ratings_matrix[1:nrow(ratings_matrix)],method="POPULAR")
+  # save the model to disk
+  saveRDS(rec, "./Popular_model.rds")
+  recom <- predict(rec, ratings_matrix[250], n=3)
+  return(as(recom, "list"))
+  # Stop the clock
+  proc.time() - ptm
+}
+
+#----------------------------------------testing the function-----------------------------------------------#
+
+#pop_predict<-Popular_train(ratings_mat)
+#pop_predict
