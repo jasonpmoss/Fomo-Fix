d4f130eb2b6e865f4587e3129248ed797f9cd9e1
RafaGuzmanNYU <46539205+RafaGuzmanNYU@users.noreply.github.com>
2019-01-09 21:10:44 +0100

    Update split_train_test_data.R
    added a second input variable to control the size of the training/test datasets

--- split_train_test_data.R
+++ split_train_test_data.R
@@ -1,10 +1,12 @@
-#DESCRIPTION: This function splits the ratings matrix into two rating matrix, one for trainin and the other for testing.
+#DESCRIPTION: This function splits the ratings matrix into two rating matrix, one for trainin and the other for testing. To do so, it is required the percentage
+#of training data that will be used. For example, if the function received train_proportion = 0.8, it will create a training dataset
+#with 80% of the whole rating matrix and a test dataset with 20% od the rating matrix.
 #Once created, all different recommenders can use the same train/test datasets to make predictions
 
-split_train_test_data <- function(ratingmat){
-  #INPUT: a realRatingMatrix containing the ratings
+split_train_test_data <- function(ratingmat, train_proportion){
+  #INPUT: a realRatingMatrix containing the ratings and train_proportion which is the percentage of training data that will be taken from the rating matrix.
   #OUTPUT: does not return anything but creates two realRatingMatrix object in the environment: recc_data_train and recc_data_test
-  which_train <- sample(x = c(TRUE, FALSE), size = nrow(ratingmat), replace = TRUE, prob = c(0.8, 0.2))
+  which_train <- sample(x = c(TRUE, FALSE), size = nrow(ratingmat), replace = TRUE, prob = c(train_proportion, 1-train_proportion))
   recc_data_train <<- ratingmat[which_train, ]
   recc_data_test <<- ratingmat[!which_train, ] 
 }
