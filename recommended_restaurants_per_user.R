9c93dda2a3f03a586bbbf8900672801bd9d9e71c
RafaGuzmanNYU <46539205+RafaGuzmanNYU@users.noreply.github.com>
2019-01-12 11:46:51 +0100

    Update recommended_restaurants_per_user.R
    Now it has two differnet functions, one that returns all recommended restaurants for a given user, and another that only returns the top "n" recommendations

--- recommended_restaurants_per_user.R
+++ recommended_restaurants_per_user.R
@@ -1,10 +1,9 @@
 #Author: Rafa
 
-#Description: this function allows filtering a list of predicted items by the name of the user for which the recommendation is done
-#INPUT: a "topNList" object containing predictions and the name of an user as a string of characters
+#Description: this function resturns all predicted items for a given user
+#INPUT: a "topNList" object containing predictions, the name of an user as a string of characters and an integer indicating the number of recommendations to return
 #OUTPUT: the list of string of characters containing the names of the restaurants recommended for that particula user
-
-index_list <- function(matrix, user){
+all_recommended_restaurants_per_user <- function(matrix, user){
   #extract the list of index of the recommended restaurants for the user
   restaurants_index <- matrix@items[[user]]
   
@@ -13,8 +12,23 @@ index_list <- function(matrix, user){
   
 }
 
+
+#Description: this function resturns the top "n" predicted items for a given user
+#INPUT: a "topNList" object containing predictions and the name of an user as a string of characters
+#OUTPUT: the list of string of characters containing the names of the restaurants recommended for that particula user
+top_n_recommended_restaurants_per_user <- function(matrix, user, num_recommendations){
+  #extract the list of index of the recommended restaurants for the user
+  restaurants_index <- matrix@items[[user]][1:num_recommendations]
+  
+  #returns the names of thoese restaurants
+  return(matrix@itemLabels[restaurants_index])
+  
+}
 #------- test ---------------------------
 
 matrix <- recc_predicted #where recc_predicted is the output of the function "predict" of a recommender model
 user <- "DTo6vHKzyx8RmMrVbTgowg"
-index_list(matrix, user)
+n <- 2
+top_n_recommended_restaurants_per_user(matrix, user, n)
+all_recommended_restaurants_per_user(matrix, user)
+
