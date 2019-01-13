4dcdca5a537a62a1d1652a92b0891fabe57ba599
Jason <jason.moss@stern.nyu.edu>
2019-01-13 14:13:56 +0200

    updated master with training and predictions. Cleaned out code on predict and train files to make consistent

--- IBCF_predict.R
+++ IBCF_predict.R
@@ -3,22 +3,18 @@
 library(plyr)
 library(recommenderlab)
 
-IBCF<-function(ratings_matrix,user_number,n_recommended){
+IBCF_predict<-function(ratings_matrix,n_recommended){
   # load the model
   IBCF_model <- readRDS("./IBCF_model.rds")
   
   # make a predictions on "new data" using the final model
-  recc_predicted <- predict(object = IBCF_model, newdata = ratings_matrix, n = n_recommended)
+  IBCF_recc_predicted <- predict(object = IBCF_model, newdata = ratings_matrix, n = n_recommended)
   
-  recc_list <- sapply(recc_predicted@items, function(x){
-    colnames(ratings_matrix)[x]
-  })
-  recc_matrix <- ldply(recc_list, rbind)
-  recc_matrix_not_empty <- recc_matrix[!is.na(recc_matrix[2]),]
-  
-  return(recc_matrix_not_empty[user_number,])
+  return(IBCF_recc_predicted)
 }
-n_recommended<-3
-user_number<-15
-IBCF_predict<-IBCF(recc_data_train, user_number, n_recommended)
-IBCF_predict
+
+#-----------------------test the function----------------------------------------------
+# n_recommended<-3
+# IBCF_predict<-IBCF(recc_data_train,n_recommended)
+# IBCF_predict
+ 
--- master.R
+++ master.R
@@ -16,6 +16,7 @@ number_of_records<-10000
 #user to be recommended for:
 #user<-ratings$small$user_id[17]
 user<-"_91FxFZYTaUGVIEnQhB5cw"
+n_recommended<-3
 #return user rating table:
 
 #----------------------------------------Main code-----------------------------------------------#
@@ -32,8 +33,23 @@ ratings_mat<-ratings_matrix(ratings$user_id, ratings$business_id, ratings$stars)
 #user_locations<-which(grepl(user, ratings$user_id))
 source("ratings_matrix.R")
 user_matrix<-ratings_matrix(user_table$user_id, user_table$business_id, user_table$stars)
-#ratings_matrix
+
+
+#train models:
+
 source("split_train_test_data.R")
 split_train_test_data(ratings_mat,0.8)
 source("IBCF_train.R")
 IBCF_train(ratings_mat)
+
+source("UBCF_train.R")
+UBCF_train(ratings_mat)
+
+#run predictions:
+
+source("IBCF_predict.R")
+IBCF_predict<-IBCF_predict(recc_data_train, n_recommended)
+
+source("UBCF_predict.R")
+UBCF_predict<-UBCF_predict(recc_data_train, n_recommended)
+
