d1d7fb3cb9570289b182f55d9d4365e4685a580a
Rafael Guzman Robles <rafaguzman@gmail.com>
2019-01-14 23:11:43 +0100

    first draft of the code to convert the predictions returned by the predict function into a matrix. In a first step, it adds the restaurants ids to the matrix, then it removes users not having any recommendation. There is a function that given an user and the recommendation matrix, returns the recommended restaurants (if any)

---
+++ transform_predictions_to_matrix.R
@@ -0,0 +1,28 @@
+library(dplyr)
+#add the restaurants ids
+recc_list <- sapply(IBCF_predict@items, function(x){
+  colnames(ratings_mat)[x]
+})
+
+
+#We store the recommendations into a matrix instead of a list, and we add "NA" to the missing values:
+recc_matrix <- ldply(recc_list, rbind)
+
+#We retrieve only those users having at least one recommendation (first recommended business different than "NA" --second column of the matrix != NA--)
+recc_matrix_not_empty <- recc_matrix[!is.na(recc_matrix[2]),]
+head(recc_matrix_not_empty)
+
+
+#To access to the recommendations for a given user, we create the following function:
+recommendations_for_user <- function(u, rec_mat){
+  result <- rec_mat[rec_mat$.id==u,-1]
+  result <- result[!is.na(result)]
+  result
+}
+
+#For example, the recommendations for the following two users
+print("Recommendations for user 0FMte0z-repSVWSJ_BaQTg:")
+recommendations_for_user("0FMte0z-repSVWSJ_BaQTg", recc_matrix_not_empty)
+print("Recommendations foruser 3nDUQBjKyVor5wV0reJChg:")
+recommendations_for_user("3nDUQBjKyVor5wV0reJChg", recc_matrix_not_empty)
+
