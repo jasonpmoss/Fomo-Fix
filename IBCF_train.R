578b661882875cb2d23de774928bd28535177fe7
Rafael Guzman Robles <rafaguzman@gmail.com>
2019-01-26 16:20:52 +0100

    added new parameters to the function

--- IBCF_train.R
+++ IBCF_train.R
@@ -1,12 +1,18 @@
 #Author: Jason
-IBCF_train<-function(ratings_matrix){
+library(dplyr)
+library(recommenderlab)
+
+IBCF_train<-function(training_data, nearest_neighbors = 25, distance = "cosine", normalize = "Center", alpha_input = 0.5, na_as_zero_input = "FALSE"){
   # Start the clock!
   ptm <- proc.time()
   
-  library(dplyr)
-  library(recommenderlab)
-  
-  recc_model <- Recommender(data = recc_data_train, method = "IBCF", parameter = list(k = 30))
+  recc_model <- Recommender(data = training_data, 
+                            method = "IBCF", 
+                            parameter = list(k = nearest_neighbors, 
+                                             method = distance, 
+                                             normalize_sim_matrix = normalize,  
+                                             alpha = alpha_input,
+                                             na_as_zero =  na_as_zero_input))
   
   # save the model to disk
   saveRDS(recc_model, "./IBCF_model.rds")
