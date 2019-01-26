9433d438b9ed82786440ca7c7c5bbee075d83eee
jasonpmoss <jm7755@stern.nyu.edu>
2019-01-26 16:24:07 +0000

    code clean up

--- master.R
+++ master.R
@@ -12,13 +12,13 @@ library(magrittr)
 # WHERE
 #   stars>0;"
 
-number_of_records<-10000
+number_of_records<-1000
 #user to be recommended for:
 
 #user<-"_ijx1PqANQVFLGNWCibdig" #this will always need to be updated because this user might not be in the sample
 n_recommended<-3
 #which(grepl("_ijx1PqANQVFLGNWCibdig", ratings$user_id))
-model_training_required <- FALSE #set this to TRUE to train new models
+model_training_required <- TRUE #set this to TRUE to train new models
 
 #----------------------------------------Create ratings matrix-----------------------------------------------
 # source("get_data.R") # has to be in the same working directory or specify full path
@@ -27,7 +27,7 @@ model_training_required <- FALSE #set this to TRUE to train new models
 # save(ratings,file="ratings.Rda") #create saved dataset so we can re-use it on the models that we've saved.
 
 load("ratings.Rda") #load presaved dataset with dataframe name "ratings"
-user<-"_7A9uRTB8MEUGrloUdo8Fw" #ratings$user_id[17] #find a random user at location 17
+user<-"JhWKw3FTZoRZ9riDd020LQ" #ratings$user_id[17] #find a random user at location 17
 source("ratings_matrix.R")
 ratings_mat<-ratings_matrix(ratings$user_id, ratings$business_id, ratings$stars)
 user_table<-ratings[ratings$user_id == user, ]
@@ -70,43 +70,25 @@ source("UBCF_predict.R")
 UBCF_predict<-UBCF_predict(recc_data_test, n_recommended)
 
 source("Popular_predict.R")
-Popular_predict<-Popular_train(ratings_mat, n_recommended)
+Popular_predict<-Popular_predict(ratings_mat, n_recommended)
 
 source("Hybrid_predict.R")
 Hybrid_predict_rec_list<-Hybrid_predict(ratings_mat)
 
-source("recommended_restaurants_per_user.R")
-top_n_recommended_restaurants_per_user(UBCF_predict,user,n_recommended)
-
-# Convert recommendations to lists (hopefully, this will ease extraction of user specific recommendations)
-IBCF_list<-as(IBCF_predict, "list")
-UBCF_list<-as(UBCF_predict, "list")
-Popular_list<-as(Popular_predict, "list")
-
-# the following code is using dummy data to get an ALTERNATE hybrid recommender working
-
-IBCF_top_n <- IBCF_list[[2]] # dummy predictions
-UBCF_top_n <- UBCF_list[[1]] # dummy predictions
-Popular_top_n <- Popular_list[[1]] # dummy predictions
-
-#-----------------------this hybrid recommender uses equal weightings with no ability to change weightings--------------------
-source("Hybrid_predict.R")
-Hybrid_predict <- Hybrid_predict_unweighted(IBCF_top_n, UBCF_top_n, Popular_top_n)
-
-#------------------------map recommendations--------------------------------------------
-source("transform_predictions_to_matrix.R")
-res_df<-recommendations_for_user(user,Hybrid_predict_rec_list)
-source("map_recommendations.R")
-res_plot(get_restaurants(res_df)) #Hybrid_predict_unweighted is a dataframe of business_ids
-
-save.image(file='variable_environment_20190118.RData')
-
+#source("recommended_restaurants_per_user.R")
+#top_n_recommended_restaurants_per_user(UBCF_predict,user,n_recommended)
 
 #-------------------------doing recommendations for a specific user-------------
 source("recommended_restaurants_per_user.R")
 #source("UBCF_train.R")
 #UBCF_train(ratings_mat)
 predictions<-predict_per_user(UBCF_model, recc_data_test, user, 10)
-user_restaurants_visits<-(subset(ratings,user_id==user))[,1]
-
+user_restaurants_visits<-(subset(ratings,user_id==user))[,2]
+predictions<-as.data.frame(predictions)
+#------------------------map recommendations--------------------------------------------
+source("map_recommendations.R")
+res_plot(get_restaurants(predictions)) #Hybrid_predict_unweighted is a dataframe of business_ids
 table_for_user<-c(predictions, user_restaurants_visits)
+table_for_user %<>% as.data.frame()
+table_for_user
+save.image(file='variable_environment_20190118.RData')
