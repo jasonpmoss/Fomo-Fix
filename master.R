c641e98d28f64b40a3f8f94f0a326230b2a42f33
jasonpmoss <jm7755@stern.nyu.edu>
2019-01-26 21:19:02 +0000

    Cleaned up comments and some variable names

--- master.R
+++ master.R
@@ -15,7 +15,6 @@ library(magrittr)
 number_of_records<-10000
 #user to be recommended for:
 
-#user<-"_ijx1PqANQVFLGNWCibdig" #this will always need to be updated because this user might not be in the sample
 n_recommended<-3
 #which(grepl("_ijx1PqANQVFLGNWCibdig", ratings$user_id))
 model_training_required <- TRUE #set this to TRUE to train new models
@@ -27,13 +26,13 @@ model_training_required <- TRUE #set this to TRUE to train new models
 # save(ratings,file="ratings.Rda") #create saved dataset so we can re-use it on the models that we've saved.
 
 load("ratings.Rda") #load presaved dataset with dataframe name "ratings"
-user<-"JhWKw3FTZoRZ9riDd020LQ" #ratings$user_id[17] #find a random user at location 17
+user<-"JhWKw3FTZoRZ9riDd020LQ" #choose a random user
 source("ratings_matrix.R")
 ratings_mat<-ratings_matrix(ratings$user_id, ratings$business_id, ratings$stars)
 user_table<-ratings[ratings$user_id == user, ]
 source("ratings_matrix.R")
 user_matrix<-ratings_matrix(user_table$user_id, user_table$business_id, user_table$stars)
-#user_locations<-which(grepl(user, ratings$user_id))
+#user_locations<-which(grepl(user, ratings$user_id)) #show the location of users in data frame
 
 #-----------------------------------------train models---------------------------------------------
 # these two lines are outside the if statement because we currently use the test set to
@@ -78,13 +77,14 @@ Hybrid_predict<-Hybrid_predict(ratings_mat)
 
 #-------------------------doing recommendations for a specific user-------------
 source("recommended_restaurants_per_user.R")
-predictions<-predict_per_user(Hybrid_mo, recc_data_test, user, 10)
-user_restaurants_visits<-(subset(ratings,user_id==user))[,1]
-predictions<-as.data.frame(predictions)
+predictions<-predict_per_user(Hybrid_model, recc_data_test, user, 10)
+user_restaurants_visited<-(subset(ratings,user_id==user))[,1]
+predictions %<>% as.data.frame()
 #------------------------map recommendations--------------------------------------------
 source("map_recommendations.R")
 res_plot(get_restaurants(predictions)) #Hybrid_predict_unweighted is a dataframe of business_ids
-table_for_user<-c(predictions, user_restaurants_visits)
+table_for_user<-c(user_restaurants_visited, predictions)
 table_for_user %<>% as.data.frame()
+names(table_for_user)<-c("restaurants_visited", "predictions")
 table_for_user
-save.image(file='variable_environment_20190118.RData')
+save.image(file='variable_environment_20190126.RData')
