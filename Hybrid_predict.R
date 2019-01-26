9f514101736831655d7922d5dfd0f74d192d0f75
Rafael Guzman Robles <rafaguzman@gmail.com>
2019-01-26 11:16:51 +0100

    change Hybrid_predict function to get the same structure than the rest of predictive functions

--- Hybrid_predict.R
+++ Hybrid_predict.R
@@ -1,16 +1,12 @@
-#two recommender models here. See notes on master.
 library(recommenderlab)
 
-Hybrid_predict<-function(recom){
-  return(as(predict(recom, recc_data_test), "topNList"))
-}
-
-Hybrid_predict_unweighted<-function(IBCF,UBCF,Popular){
-  final_recommendations <- c(IBCF, UBCF, Popular)
-  final_recommendations <- unique(unlist(strsplit(final_recommendations, " "))) # bring back unique items only
-  final_recommendations %<>% data.frame(stringsAsFactors = FALSE)
-  names(final_recommendations) <- "business_id"
-  user_restaurants_visits<-(subset(ratings,user_id==user))[,1] #restaurants the user has previously visited
-  final_recommendations <- anti_join(final_recommendations,user_restaurants_visits) # eliminate restaurants the user has previously visited
-  return (final_recommendations)
+Hybrid_predict<-function(ratings_matrix){
+  
+  # load the model
+  Hybrid_model <- readRDS("./Hybrid_model.rds")
+  
+  Hybrid_recc_predicted <- predict(Hybrid_model, ratings_matrix)
+  
+  return(Hybrid_recc_predicted)
+  # return(as(Hybrid_recc_predicted, "topNList"))
 }
