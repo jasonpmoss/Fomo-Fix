717b11a7a4d3e1c5286cdc23b1533b4a0bca4dd1
Rafael Guzman Robles <rafaguzman@gmail.com>
2019-02-02 17:01:34 +0100

    comment added

--- master_deprecated.R
+++ master_deprecated.R
@@ -82,6 +82,7 @@ source("recommended_restaurants_per_user.R")
 #get  predicted ratings from top n restaurants. n can be passed as parameter, otherwise its value by default is 100
 predicted_ratings<-predict_ratings_per_user(UBCF_model, recc_data_test, user, 10)
 predicted_ratings
+# predicted_ratings$Restaurant to see only the restaurants name 
 
 user_restaurants_visited<-(subset(ratings,user_id==user))[,1]
 predictions %<>% as.data.frame()
