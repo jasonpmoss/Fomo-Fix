4cde4f090768401200675a20a8b8c1214b9c33e0
Jason <jason.moss@stern.nyu.edu>
2019-02-03 16:36:14 +0200

    Cleaning up variable names

--- Hybrid_predict.R
+++ Hybrid_predict.R
@@ -10,7 +10,7 @@ Hybrid_predict_restaurants<-function(ratings_matrix){
   return(Hybrid_recc_predicted)
 }
 
-Hybrid_predict_restaurants<-function(ratings_matrix){
+Hybrid_predict_ratings<-function(ratings_matrix){
   
   # load the model
   Hybrid_model <- readRDS("./Hybrid_model.rds")
