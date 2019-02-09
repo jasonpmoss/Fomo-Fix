11a90427ed2957ed5db544af4798752c4130d65c
Rafael Guzman Robles <rafaguzman@gmail.com>
2019-02-09 09:14:55 +0100

    few adjustments and comments

--- Evaluation_all_models.R
+++ Evaluation_all_models.R
@@ -29,8 +29,25 @@ algorithms <- list("UBCF_N_C" = list(name="UBCF", param=list(normalize = NULL, m
                    
 eval_results <- evaluate(scheme, algorithms, type="topNList", n=c(1, 5, 10, 15, 20, 50, 100))
 
-plot(eval_results, annotate=c(1,3), legend="bottomright", main = "ROC curve") # Receiver Operating Characteristic (ROC) Curve
+plot(eval_results, annotate=c(1,3), legend="topleft", main = "ROC curve") # Receiver Operating Characteristic (ROC) Curve
+#NOTE:A model with no skill at each threshold is represented by a diagonal line from 
+#the bottom left of the plot to the top right and has an AUC of 0.5 --> JUST OUR CASE!!
 
 plot(eval_results, "prec/rec", annotate=TRUE, main = "Precision-recall", legend="topleft") # Precision-Recall (P-R) Curves
+#NOTE: A model with perfect skill is depicted as a point at [1.0,1.0]. 
+#A skilful model is represented by a curve that bows towards [1.0,1.0] above the flat line of no skill.
+#JUST THE OPPOSITE TO OUR CASE!!!
 
-getConfusionMatrix(eval_results)[[1]]
+#we can display the results of the evaluation of one model, including all the splits:
+getConfusionMatrix(eval_results$UBCF_N_C)
+
+#If we want to take account of all the splits at the same time, we can just sum up the indices:
+columns_to_sum <- c("TP", "FP", "FN", "TN")
+indices_summed <- Reduce("+", getConfusionMatrix(eval_results$UBCF_N_C))[,columns_to_sum]
+head(indices_summed)
+
+#or just compute the average of them
+avg(eval_results$UBCF_N_C)
+
+#for all results: 
+#avg(eval_results)
