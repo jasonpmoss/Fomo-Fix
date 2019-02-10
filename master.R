454762070b7166cdc7d2ac8ac29a0077e6848959
Rafael Guzman Robles <rafaguzman@gmail.com>
2019-02-10 18:00:02 +0100

    small change

--- master.R
+++ master.R
@@ -57,7 +57,7 @@ source("predictions_on_test_data.R")
 
 #get  predicted ratings from top n restaurants. n can be passed as parameter, otherwise its value by default is 100
 source("recommended_restaurants_per_user.R")
-predicted_ratings<-predict_ratings_per_user(Hybrid_model, ratings_mat, user, 20)
+predicted_ratings<-predict_ratings_per_user(Popular_model, ratings_mat, user, 20)
 predictions<-predicted_ratings$Restaurant #to see only the restaurants name 
 user_restaurants_visited<-(subset(ratings,user_id==user))[,1]
 predictions %<>% as.data.frame()
