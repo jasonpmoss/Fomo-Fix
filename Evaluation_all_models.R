50c42355e7ed077466d58d76ab5187d73203dbe1
Rafael Guzman Robles <rafaguzman@gmail.com>
2019-04-19 13:12:29 +0200

    new models added

--- Evaluation_all_models.R
+++ Evaluation_all_models.R
@@ -3,20 +3,60 @@ library(recommenderlab)
 #we already have the evaluationScheme saved in the eval_set variable. We will use it
 scheme <- evaluationScheme(ratings_mat, method="cross-validation", k=4, given=-1, goodRating=3.5)
 
-algorithms <- list("UBCF_N_C" = list(name="UBCF", param=list(normalize = NULL, method="Cosine")),
-                   "UBCF_C_C" = list(name="UBCF", param=list(normalize = "center", method="Cosine")),
-                   "UBCF_Z_C" = list(name="UBCF", param=list(normalize = "Z-score", method="Cosine")),
-                   "UBCF_N_E" = list(name="UBCF", param=list(normalize = NULL, method="Euclidean")),
-                   "UBCF_C_E" = list(name="UBCF", param=list(normalize = "center", method="Euclidean")),
-                   "UBCF_Z_E" = list(name="UBCF", param=list(normalize = "Z-score", method="Euclidean")),
-                   "UBCF_N_J" = list(name="UBCF", param=list(normalize = NULL, method="Jaccard")),
-                   "UBCF_C_J" = list(name="UBCF", param=list(normalize = "center", method="Jaccard")),
-                   "UBCF_Z_J" = list(name="UBCF", param=list(normalize = "Z-score", method="Jaccard")),
-                   "UBCF_N_C_5" = list(name="UBCF", param=list(normalize = NULL, method="Cosine", nn=5)),
-                   "UBCF_N_C_10" = list(name="UBCF", param=list(normalize = NULL, method="Cosine", nn=10)),
-                   "UBCF_N_C_15" = list(name="UBCF", param=list(normalize = NULL, method="Cosine", nn=15)),
-                   "UBCF_N_C_50" = list(name="UBCF", param=list(normalize = NULL, method="Cosine", nn=50)),
-                   "UBCF_N_C_100" = list(name="UBCF", param=list(normalize = NULL, method="Cosine", nn=100)),
+algorithms <- list("UBCF_N_C_5" = list(name= "UBCF", param=list(normalize = NULL, method="Cosine", nn=5)),
+                   "UBCF_N_C_10" = list(name= "UBCF", param=list(normalize = NULL,method="Cosine", nn=10)),
+                   "UBCF_N_C_15" = list(name= "UBCF", param=list(normalize = NULL,method="Cosine", nn=15)),
+                   "UBCF_N_C_25" = list(name= "UBCF", param=list(normalize = NULL, method="Cosine", nn=25)),
+                   "UBCF_N_C_50" = list(name= "UBCF", param=list(normalize = NULL,method="Cosine", nn=50)),
+                   "UBCF_N_C_100" = list(name= "UBCF", param=list(normalize = NULL,method="Cosine", nn=100)),
+                   "UBCF_N_E_5" = list(name= "UBCF", param=list(normalize = NULL, method="Euclidean", nn=5)),
+                   "UBCF_N_E_10" = list(name= "UBCF", param=list(normalize = NULL,method="Euclidean", nn=10)),
+                   "UBCF_N_E_15" = list(name= "UBCF", param=list(normalize = NULL,method="Euclidean", nn=15)),
+                   "UBCF_N_E_25" = list(name= "UBCF", param=list(normalize = NULL, method="Euclidean", nn=25)),
+                   "UBCF_N_E_50" = list(name= "UBCF", param=list(normalize = NULL,method="Euclidean", nn=50)),
+                   "UBCF_N_E_100" = list(name= "UBCF", param=list(normalize = NULL,method="Euclidean", nn=100)),
+                   "UBCF_N_J_5" = list(name= "UBCF", param=list(normalize = NULL, method="Jaccard", nn=5)),
+                   "UBCF_N_J_10" = list(name= "UBCF", param=list(normalize = NULL,method="Jaccard", nn=10)),
+                   "UBCF_N_J_15" = list(name= "UBCF", param=list(normalize = NULL,method="Jaccard", nn=15)),
+                   "UBCF_N_J_25" = list(name= "UBCF", param=list(normalize = NULL, method="Jaccard", nn=25)),
+                   "UBCF_N_J_50" = list(name= "UBCF", param=list(normalize = NULL,method="Jaccard", nn=50)),
+                   "UBCF_N_J_100" = list(name= "UBCF", param=list(normalize = NULL,method="Jaccard", nn=100)),
+                   "UBCF_C_C_5" = list(name= "UBCF", param=list(normalize = "center", method="Cosine", nn=5)),
+                   "UBCF_C_C_10" = list(name= "UBCF", param=list(normalize = "center",method="Cosine", nn=10)),
+                   "UBCF_C_C_15" = list(name= "UBCF", param=list(normalize = "center",method="Cosine", nn=15)),
+                   "UBCF_C_C_25" = list(name= "UBCF", param=list(normalize = "center", method="Cosine", nn=25)),
+                   "UBCF_C_C_50" = list(name= "UBCF", param=list(normalize = "center",method="Cosine", nn=50)),
+                   "UBCF_C_C_100" = list(name= "UBCF", param=list(normalize = "center",method="Cosine", nn=100)),
+                   "UBCF_C_E_5" = list(name= "UBCF", param=list(normalize = "center", method="Euclidean", nn=5)),
+                   "UBCF_C_E_10" = list(name= "UBCF", param=list(normalize = "center",method="Euclidean", nn=10)),
+                   "UBCF_C_E_15" = list(name= "UBCF", param=list(normalize = "center",method="Euclidean", nn=15)),
+                   "UBCF_C_E_25" = list(name= "UBCF", param=list(normalize = "center", method="Euclidean", nn=25)),
+                   "UBCF_C_E_50" = list(name= "UBCF", param=list(normalize = "center",method="Euclidean", nn=50)),
+                   "UBCF_C_E_100" = list(name= "UBCF", param=list(normalize = "center",method="Euclidean", nn=100)),
+                   "UBCF_C_J_5" = list(name= "UBCF", param=list(normalize = "center", method="Jaccard", nn=5)),
+                   "UBCF_C_J_10" = list(name= "UBCF", param=list(normalize = "center",method="Jaccard", nn=10)),
+                   "UBCF_C_J_15" = list(name= "UBCF", param=list(normalize = "center",method="Jaccard", nn=15)),
+                   "UBCF_C_J_25" = list(name= "UBCF", param=list(normalize = "center", method="Jaccard", nn=25)),
+                   "UBCF_C_J_50" = list(name= "UBCF", param=list(normalize = "center",method="Jaccard", nn=50)),
+                   "UBCF_C_J_100" = list(name= "UBCF", param=list(normalize = "center",method="Jaccard", nn=100)),
+                   "UBCF_Z_C_5" = list(name= "UBCF", param=list(normalize = "Z-score", method="Cosine", nn=5)),
+                   "UBCF_Z_C_10" = list(name= "UBCF", param=list(normalize = "Z-score",method="Cosine", nn=10)),
+                   "UBCF_Z_C_15" = list(name= "UBCF", param=list(normalize = "Z-score",method="Cosine", nn=15)),
+                   "UBCF_Z_C_25" = list(name= "UBCF", param=list(normalize = "Z-score", method="Cosine", nn=25)),
+                   "UBCF_Z_C_50" = list(name= "UBCF", param=list(normalize = "Z-score",method="Cosine", nn=50)),
+                   "UBCF_Z_C_100" = list(name= "UBCF", param=list(normalize = "Z-score",method="Cosine", nn=100)),
+                   "UBCF_Z_E_5" = list(name= "UBCF", param=list(normalize = "Z-score", method="Euclidean", nn=5)),
+                   "UBCF_Z_E_10" = list(name= "UBCF", param=list(normalize = "Z-score",method="Euclidean", nn=10)),
+                   "UBCF_Z_E_15" = list(name= "UBCF", param=list(normalize = "Z-score",method="Euclidean", nn=15)),
+                   "UBCF_Z_E_25" = list(name= "UBCF", param=list(normalize = "Z-score", method="Euclidean", nn=25)),
+                   "UBCF_Z_E_50" = list(name= "UBCF", param=list(normalize = "Z-score",method="Euclidean", nn=50)),
+                   "UBCF_Z_E_100" = list(name= "UBCF", param=list(normalize = "Z-score",method="Euclidean", nn=100)),
+                   "UBCF_Z_J_5" = list(name= "UBCF", param=list(normalize = "Z-score", method="Jaccard", nn=5)),
+                   "UBCF_Z_J_10" = list(name= "UBCF", param=list(normalize = "Z-score",method="Jaccard", nn=10)),
+                   "UBCF_Z_J_15" = list(name= "UBCF", param=list(normalize = "Z-score",method="Jaccard", nn=15)),
+                   "UBCF_Z_J_25" = list(name= "UBCF", param=list(normalize = "Z-score", method="Jaccard", nn=25)),
+                   "UBCF_Z_J_50" = list(name= "UBCF", param=list(normalize = "Z-score",method="Jaccard", nn=50)),
+                   "UBCF_Z_J_100" = list(name= "UBCF", param=list(normalize = "Z-score",method="Jaccard", nn=100)),
                    "IBCF_N_C" = list(name="IBCF", param=list(normalize = NULL, method="Cosine")),
                    "IBCF_C_C" = list(name="IBCF", param=list(normalize = "center", method="Cosine")),
                    "IBCF_Z_C" = list(name="IBCF", param=list(normalize = "Z-score", method="Cosine")),
@@ -44,17 +84,17 @@ algorithms <- list("UBCF_N_C" = list(name="UBCF", param=list(normalize = NULL, m
   
 eval_results <- evaluate(scheme, algorithms, type="topNList", n=c(1, 5, 10, 15, 20, 50, 100))
 
-plot(eval_results, annotate=c(1,3), legend="topleft", main = "ROC curve") # Receiver Operating Characteristic (ROC) Curve
+# plot(eval_results, annotate=c(1,3), legend="topleft", main = "ROC curve") # Receiver Operating Characteristic (ROC) Curve
 #NOTE:A model with no skill at each threshold is represented by a diagonal line from 
 #the bottom left of the plot to the top right and has an AUC of 0.5 --> JUST OUR CASE!!
 
-plot(eval_results, "prec/rec", annotate=TRUE, main = "Precision-recall", legend="topleft") # Precision-Recall (P-R) Curves
+# plot(eval_results, "prec/rec", annotate=TRUE, main = "Precision-recall", legend="topleft") # Precision-Recall (P-R) Curves
 #NOTE: A model with perfect skill is depicted as a point at [1.0,1.0]. 
 #A skilful model is represented by a curve that bows towards [1.0,1.0] above the flat line of no skill.
 #JUST THE OPPOSITE TO OUR CASE!!!
 
 #we can display the results of the evaluation of one model, including all the splits:
-avg(eval_results)
+# avg(eval_results)
 
 #or we can observe each confusion matrix separetly. 
 #Note: if we use k-fold then we get "k" confusion matrix per model, one per fold  
