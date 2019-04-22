469b97577e64c21bfe9cdc3a362a7cb5e6272229
Jason <jason.moss@stern.nyu.edu>
2019-04-22 17:35:48 +0200

    first revision of hybrid evaluation

--- Evaluate_recommendations_hybrid.R
+++ Evaluate_recommendations_hybrid.R
@@ -1,65 +1,77 @@
 ### Function to evaluate the hybrid model
-get_Hybrid_Eval <- function(p_act_and_pred) {
-  tb_tst_visited <- table(p_act_and_pred[, c("visited", 
-                                             "visit_pred")])
+library(ModelMetrics)
+library(data.table)
+library(knitr)
+
+get_Hybrid_Eval <- function(actual_rating, predicted_rating, actual_visit) {
   
-  #View(tb_tst_visited)
+  mse<- MSE(actual_rating, predicted_rating)
+  mae<- MAE(actual_rating, predicted_rating)
+  rmse<-RMSE(actual_rating, predicted_rating)
+
+  
+  threshold<-3.49
+  
+  eval_df<-data.frame(actual_rating, predicted_rating, actual_visit)
+  eval_df$predicted_visit<-ifelse(eval_df[,2]>threshold,1,0)
+  tb_tst_visited <- confusionMatrix(actual_visit, eval_df$predicted_visit)
+  
   tn <- tb_tst_visited[1,1]
   fp <- tb_tst_visited[1,2]
   fn <- tb_tst_visited[2,1]
   tp <- tb_tst_visited[2,2]
   
-  #(sensitivity*100)% of the times a user visited a restaurant, we correctly classified it as a visit.
-  sensitivity <- tp / (tp + fn) #This is recall / tpr
-  #(specificity*100)% of the "no-visits" by users to a restaurant have been correctly identified by our model.
-  specificity <- tn / (tn + fp) #This is tnr
+  recall <- recall(actual_visit, eval_df$predicted_visit) #TPR
+  specificity <- tnr(actual_visit, eval_df$predicted_visit) #TNR
   
+  #accuracy <- 1- ce(actual_visit, eval_df$predicted_visit)
   
-  #Classification accuracy
-  classi_accu <- ((tp + tn ) / (tp + tn+ fp + fn) )
+  #classi_error <- ce(actual_visit, eval_df$predicted_visit)
   
-  #Classification error
-  classi_error <- 1 - classi_accu
+  precision <- ppv(actual_visit, eval_df$predicted_visit)
   
-  #Precision
-  #When a positive value is predicted, how often is the prediction correct?
-  #How "precise" is the classifier when predicting positive instances?
+  output<- as.data.frame(matrix(ncol=8,nrow=1))
+  colnames(output) <- c("TP","FP","FN","TN","Precision","Recall","TPR","FPR")
+  modtypes<-c("Results")
+  (setattr(output, "row.names", modtypes))
+
+  output["Results","TP"] <- tp
+  output["Results","FP"] <- fp
+  output["Results","FN"] <- fn
+  output["Results","TN"] <- tn
+  output["Results","Precision"] <- precision
+  output["Results","Recall"] <- recall
+  output["Results","TPR"] <- specificity
+  output["Results","FPR"] <- 1-specificity
   
-  precision <- tp / (tp + fp)
+  hybrid_eval_recommendations<<-kable(output, caption="Results")
   
-  df <- data.frame(matrix(ncol = 2,
-                          nrow = 0 ))
-  
-  names(df) <- c("Metric", "Value")
-  
-  df <- rbind(df,
-              list("TP", tp))
-  df <- rbind(df,
-              list("TN", tn))
-  df <- rbind(df,
-              list("FP", fp))
-  df <- rbind(df,
-              list("FN", fn))
-  df <- rbind(df,
-              list("Recall", sensitivity))
-  df <- rbind(df,
-              list("Specificity", specificity))
-  df <- rbind(df,
-              list("Classification Accuracy", classi_accu))
-  df <- rbind(df,
-              list("Classification Error", classi_error))
-  df <- rbind(df,
-              list("Precision", precision))
-  
-  df
+  output2<- as.data.frame(matrix(ncol=3,nrow=1))
+  colnames(output2) <- c("RMSE","MSE","MAE")
+  modtypes2<-c("Results")
+  (setattr(output2, "row.names", modtypes2))
+  output2["Results","RMSE"] <- rmse
+  output2["Results","MSE"] <- mse
+  output2["Results","MAE"] <- mae
+  hybrid_eval_ratings <<-   kable(output2, caption="Results")
 }
 
 #--------------------------- test function --------------------------------------------------------------------
 #Evaluate
 
 actual_rating_df <- as(ratings_mat, "data.frame") # Converting the rating matrix into dataframe 
-pred_rating_df <- as(hybrid_ratings, "data.frame")# converting predicted rating matrix into dataframe
-merge_data<- merge(actual_rating_df, pred_rating_df, by=c("user","item"))# merge the ratings by user and business combination
-merge_data<-subset(merge_data, select = 3:4)
-names(merge_data) <- c("visited", "visit_pred") # rename the column name
-get_Hybrid_Eval(merge_data)
+pred_rating_df <- sample.int(5,size=5420, replace=TRUE) #as(hybrid_ratings, "data.frame")# converting predicted rating matrix into dataframe
+MSE(actual_rating_df[,3], pred_rating_df)
+MAE(actual_rating_df[,3], pred_rating_df)
+RMSE(actual_rating_df[,3], pred_rating_df)
+actual_visit <- sample(0:1, replace=TRUE, size = 5420)
+eval_df<-data.frame(actual_rating_df[,3], pred_rating_df, actual_visit)
+eval_df$predicted_visit<-ifelse(eval_df[,2]>threshold,1,0)
+#table(eval_df$actual_visit, eval_df$predicted_visit)
+#actual_visit
+#table(eval_df[,3])
+#confusionMatrix(actual_rating_df[,3], pred_rating_df, cutoff = 3.5)
+
+get_Hybrid_Eval(eval_df[,1], eval_df[,2],eval_df[,3])
+hybrid_eval_recommendations
+hybrid_eval_ratings
