96179b53f4382dc30797f49b5d647064190c01fd
Jason <jason.moss@stern.nyu.edu>
2019-04-27 22:31:10 +0200

    Update model parameters for Hybrid

--- model_training_hybrid.R
+++ model_training_hybrid.R
@@ -1,7 +1,7 @@
 # #Train the Hybrid model
 
 source("Hybrid_train.R")
-Hybrid_train(UBCF_C_E_100, IBCF_N_E, Popular_C, Popular_sentiment_model, 0.3, 0.3, 0.3, 0.1)
+Hybrid_train(UBCF_N_J_100, IBCF_N_C_100, Popular_N, Popular_sentiment_N, 0.5, 0, 0.25, 0.25)
 print("Hybrid training Done")
 Hybrid_model <- readRDS("./hybrid_model.rds")
 gc()
